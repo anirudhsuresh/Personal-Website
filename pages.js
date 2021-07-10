c2c70f6caf494690c2b28432ae5f909c789f8a05
Anirudh <aniramch@ucdavis.edu>
2021-07-10 14:09:29 -0700

    made changes

---
+++ pages.js
@@ -0,0 +1,16 @@
+const ghpages = require("gh-pages");
+const pathname = `${__dirname}/build`;
+// const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/anirudhsuresh/anirudhsuresh.github.io.git";
+
+ghpages.publish(
+  pathname,
+  {
+    branch: "master",
+    repo: repoURL,
+  },
+  (err) => {
+    if (err) console.log("ERROR: ", err);
+    else console.log("PUBLISHED");
+  }
+);
